07786a1bf88ddb1331907a49a8b5d6c213531a52
Patrick Morgan <patrickmorgan20@gmail.com>
2024-10-18 15:55:11 -0400

    clean up some hankey pankey

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
-import config from "./config";
+import { getAuth, User } from "firebase/auth";
 import { logEvent } from "./analytics";
+import config from "./config";
 
 const firebaseConfig = config;
 
@@ -11,23 +11,23 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-let authStateInitialized = false;
-let currentUser: User | null = null;
+// let authStateInitialized = false;
+// let currentUser: User | null = null;
 
 // Initialize the auth state
-const initializeAuthState = (): Promise<void> => {
-  return new Promise((resolve) => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      currentUser = user;
-      authStateInitialized = true;
-      if (user) {
-        logEvent("user_authenticated", { user_id: user.uid });
-      }
-      unsubscribe();
-      resolve();
-    });
-  });
-};
+// const initializeAuthState = (): Promise<void> => {
+//   return new Promise((resolve) => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       currentUser = user;
+//       authStateInitialized = true;
+//       if (user) {
+//         logEvent("user_authenticated", { user_id: user.uid });
+//       }
+//       unsubscribe();
+//       resolve();
+//     });
+//   });
+// };
 
 export const signInWithChrome = (): Promise<User> => {
   return new Promise((resolve, reject) => {
@@ -37,7 +37,7 @@ export const signInWithChrome = (): Promise<User> => {
       } else if (response.error) {
         reject(new Error(response.error));
       } else {
-        currentUser = response.user;
+        // currentUser = response.user;
         resolve(response.user);
       }
     });
@@ -69,37 +69,6 @@ export const getIdToken = async (): Promise<string | null> => {
   return null;
 };
 
-// export const getCurrentUser = async (): Promise<User | null> => {
-//   if (!authStateInitialized) {
-//     await initializeAuthState();
-//   }
-//   return currentUser;
-// };
-
-// export const getIdToken = async (): Promise<string | null> => {
-//   const user = await getCurrentUser();
-//   if (user) {
-//     return new Promise((resolve, reject) => {
-//       chrome.runtime.sendMessage({ action: "getIdToken" }, (response) => {
-//         if (chrome.runtime.lastError) {
-//           logEvent("get_id_token_error", {
-//             error: chrome.runtime.lastError.message,
-//           });
-//           reject(chrome.runtime.lastError);
-//         } else if (response.error) {
-//           logEvent("get_id_token_error", {
-//             error: response.error,
-//           });
-//           reject(new Error(response.error));
-//         } else {
-//           resolve(response.token);
-//         }
-//       });
-//     });
-//   }
-//   return null;
-// };
-
 export const signOut = (): Promise<void> => {
   return new Promise((resolve, reject) => {
     chrome.runtime.sendMessage({ action: "signOut" }, (response) => {
@@ -108,7 +77,7 @@ export const signOut = (): Promise<void> => {
       } else if (response.error) {
         reject(new Error(response.error));
       } else {
-        currentUser = null;
+        // currentUser = null;
         resolve();
       }
     });
